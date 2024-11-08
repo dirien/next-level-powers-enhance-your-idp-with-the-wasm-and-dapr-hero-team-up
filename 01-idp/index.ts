f4b29b4e22086cde57205d617df29177e293333a
Engin Diri <engin.diri@ediri.de>
2024-11-08 21:50:41 +0100

    feat: add Pulumi IaC
    feat: add Pulumi IaC
    
    feat: add Pulumi IaC
    
    feat: add Pulumi IaC

---
+++ 01-idp/index.ts
@@ -0,0 +1,9 @@
+import * as pulumi from "@pulumi/pulumi";
+import {ArgoCD} from "./argocd";
+
+let initialObjects = new pulumi.asset.FileAsset("./argocd-initial-objects.yaml");
+
+const argocd = new ArgoCD("argocd", {
+    initialObjects: initialObjects,
+    version: "7.7.0",
+})
